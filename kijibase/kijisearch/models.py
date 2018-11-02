1fb9c65cbc710e9d6ebecb481e91d44f95edeb29
Anthony Quivers <anthonyquivers@gmail.com>
2018-11-01 22:22:03 -0400

    Program and Camps Tables

--- kijibase/kijisearch/models.py
+++ kijibase/kijisearch/models.py
@@ -2,8 +2,45 @@ from django.db import models
 
 # Create your models here.
 
-class Programs(models.Model)
-	pass
+class Programs(models.Model):
+	name = models.TextField()
+	address = models.CharField(max_length=400)
+	city = models.CharField(max_length=400)
+	state = models.CharField(max_length=400)
+	zip_code = models.CharField(max_length=400)
+	phone = models.CharField(max_length=400)
+	website = models.CharField(max_length=400)
+	email = models.CharField(max_length=400)
+	grades = models.CharField(max_length=400)
+	ages = models.CharField(max_length=400)
+	overview = models.TextField()
+	daily_model = models.CharField(max_length=400)
+	time_of_year = models.CharField(max_length=400)
+	focus_areas = models.TextField()
+	cost = models.CharField(max_length=400)
+	eligibility = models.TextField()
+	other_locations_of_operation = models.TextField()
+	profile_pic_link = models.CharField(max_length=400) 
 
-class Camps(models.Model)
-	pass
+class Camps(models.Model):
+	name = models.TextField()
+	course = models.TextField()
+	address = models.CharField(max_length=400)
+	city = models.CharField(max_length=400)
+	state = models.CharField(max_length=400)
+	zip_code = models.CharField(max_length=400) 
+	phone = models.CharField(max_length=400)
+	website = models.CharField(max_length=400)
+	email = models.CharField(max_length=400)
+	grades = models.CharField(max_length=400)
+	ages = models.CharField(max_length=400)
+	overview = models.TextField()
+	time_of_year = models.CharField(max_length=400)
+	schedule = models.CharField(max_length=400)
+	daily_model = models.CharField(max_length=400)
+	focus_areas = models.TextField()
+	cost = models.CharField(max_length=400)
+	register = models.CharField(max_length=400)
+	camp_image_link = models.CharField(max_length=400)
+	camp_video_link = models.CharField(max_length=400)
+
